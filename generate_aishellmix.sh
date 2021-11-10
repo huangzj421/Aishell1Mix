b469a8df94fe928de0c11c36faf5fab92a4dbff9
huangzj421 <73161104+huangzj421@users.noreply.github.com>
2021-11-10 21:47:01 +0800

    Update generate_aishellmix.sh

--- generate_aishellmix.sh
+++ generate_aishellmix.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 set -eu  # Exit on error
 
-librispeech_dir=/mnt/huangzijian/aishell1/wav
-librispeech_md_dir=/mnt/huangzijian/aishell1/metadata
-wham_dir=/mnt/huangzijian/librimix/wham_noise
-wham_md_dir=/mnt/huangzijian/librimix/LibriMix-master/metadata/Wham_noise
-metadata_dir=/mnt/huangzijian/aishellmix/metadata
-librimix_outdir=/mnt/huangzijian/aishellmix
+aishell1_dir=/youraishell1path/aishell1/wav
+aishell1_md_dir=/youraishell1path/aishell1/metadata
+wham_dir=/yourwhampath/wham_noise
+wham_md_dir=/metadata/Wham_noise
+metadata_dir=/youraishellmixpath/aishellmix/metadata
+aishellmix_outdir=/youraishellmixpath/aishellmix
 
 # If you wish to rerun this script in the future please comment this line out.
 python scripts/augment_train_noise.py --wham_dir $wham_dir
 
 for n_src in 2 3; do
-  metadata_outdir=$metadata_dir/Libri$n_src"Mix"
-  python scripts/create_aishellmix_metadata.py --librispeech_dir $librispeech_dir \
-    --librispeech_md_dir $librispeech_md_dir \
+  metadata_outdir=$metadata_dir/Aishell1"Mix"$n_src
+  python scripts/create_aishellmix_metadata.py --aishell1_dir $aishell1_dir \
+    --aishell1_md_dir $aishell1_md_dir \
     --wham_dir $wham_dir \
     --wham_md_dir $wham_md_dir \
     --metadata_outdir $metadata_outdir \
     --n_src $n_src
 
-  python scripts/create_aishellmix_from_metadata.py --librispeech_dir $librispeech_dir \
+  python scripts/create_aishellmix_from_metadata.py --aishell1_dir $aishell1_dir \
     --wham_dir $wham_dir \
     --metadata_dir $metadata_outdir \
-    --librimix_outdir $librimix_outdir \
+    --aishellmix_outdir $aishellmix_outdir \
     --n_src $n_src \
     --freqs 8k 16k \
     --modes max min \
